3797548d0b12f19765599db683c4c672786c0c45
Jacob Lewinski <j1lewinski@gmail.com>
2024-12-13 21:14:04 -0600

    day 8 pt 2

--- 2024/day8/index.ts
+++ 2024/day8/index.ts
@@ -5,26 +5,28 @@ interface Point {
     frequency?: string;
 }
 
+let maxX = 0, maxY = 0;
+
 export default function main(input: string) {
     const map = input.split('\r\n').map((line, i) => line.split('').map((x, j) => {
         return { x: j, y: i, hasAntinode: false, frequency: x !== '.' ? x : undefined };
     }));
 
     const flattened = map.flat().filter(x => x.frequency !== undefined);
-    const maxX = map[0].length;
-    const maxY = map.length;
+    maxX = map[0].length;
+    maxY = map.length;
     console.log(maxX, maxY);
 
     const distinctFrequencies = new Set<string>();
     flattened.forEach(x => distinctFrequencies.add(x.frequency!));
     distinctFrequencies.forEach(frequency => {
-        let points = flattened.filter(x => x.frequency === frequency);
+        const points = flattened.filter(x => x.frequency === frequency);
         const antiPoints = points.map((a, i) => {
             return points.filter((_, j) => i !== j).map(b => {
                 const pt = calculateA(a, b);
                 return pt;
             });
-        }).flat().filter(pt => pt.x >= 0 && pt.y >= 0 && pt.x < maxX && pt.y < maxY);
+        }).flat().flat().filter(pt => pt.x >= 0 && pt.y >= 0 && pt.x < maxX && pt.y < maxY);
         antiPoints.forEach(pt => {
             map[pt.y][pt.x].hasAntinode = true;
         });
@@ -37,9 +39,20 @@ export default function main(input: string) {
 }
 
 function calculateA(a: Point, b: Point) {
-    const pt = { x: a.x - b.x, y: a.y - b.y };
-    pt.x = a.x + pt.x;
-    pt.y = a.y + pt.y;
-    console.log('after', pt);
-    return pt;
+    const distance = { x: a.x - b.x, y: a.y - b.y };
+    const results = [];
+    let pt = { x: a.x, y: a.y };
+    while (pt.x >= 0 && pt.y >= 0 && pt.x < maxX && pt.y < maxY) {
+        results.push({ x: pt.x, y: pt.y });
+        pt.x = pt.x + distance.x;
+        pt.y = pt.y + distance.y;
+    }
+
+    pt = { x: b.x, y: b.y };
+    while (pt.x >= 0 && pt.y >= 0 && pt.x < maxX && pt.y < maxY) {
+        results.push({ x: pt.x, y: pt.y });
+        pt.x = pt.x - distance.x;
+        pt.y = pt.y - distance.y;
+    }
+    return results;
 }
